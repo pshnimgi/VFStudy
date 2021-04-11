5cf47cb3985c828f66edaef5dd8e91524db244fb
pshnimgi <pshnimgo@gmail.com>
2021-04-11 16:53:22 +0900

    Starting appbar coding

--- src/App.vue
+++ src/App.vue
@@ -5,56 +5,27 @@
       color="primary"
       dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
+      <v-app-bar-nav-icon to="/"></v-app-bar-nav-icon>
+      <v-toolbar-title>Vue&Firebase Study</v-toolbar-title>
       <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+      <!-- button link setting -->
+      <v-btn icon to="/about">
+        <v-icon>mdi-magnify</v-icon>
       </v-btn>
     </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+    
+    <!-- The area in contents: use router -->
+    <v-content>
+      <router-view></router-view>
+    </v-content>
   </v-app>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
 
 export default {
-  name: 'App',
+  name: 'App'
 
-  components: {
-    HelloWorld
-  },
-
-  data: () => ({
-    //
-  })
 }
 </script>
