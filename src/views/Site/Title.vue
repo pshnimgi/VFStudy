ca43916ba3d56281fefd60b405f48066107654d1
pshnimgi <pshnimgo@gmail.com>
2021-04-17 15:00:02 +0900

    commit

---
+++ src/views/Site/Title.vue
@@ -0,0 +1,13 @@
+// Only one tag in template tag
+<template>
+    <toolbar-title>
+        <!-- Page Title ######  -->
+        {{ title }}
+    </toolbar-title>
+</template>
+
+<script>
+export default {
+  props: ['title']
+}
+</script>
