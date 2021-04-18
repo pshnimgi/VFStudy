4b21ad4a54c3a4a0ae621490361916c21ff91626
pshnimgi <pshnimgo@gmail.com>
2021-04-18 16:26:21 +0900

    SideMenu&FirebaseSettingTest 20210418

--- src/views/Site/Menu.vue
+++ src/views/Site/Menu.vue
@@ -1,38 +1,90 @@
 <template>
-  <div>
-    <!-- SubSideMenus -->
-      <v-list
-        dense
-        nav
-      >
-        <v-list-item
-          v-for="item in items"
-          :key="item.title"
-          link
-        >
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
+  <v-card
+    max-width="400"
+    class="mx-auto"
+  >
+    <v-toolbar
+      color="teal"
+      dark
+    >
+      <!-- <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
+      <v-toolbar-title>Topics</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn> -->
+    </v-toolbar>
+
+    <v-list>
+      <!-- <v-list-group
+        v-for="item in items"
+        :key="item.title"
+        v-model="item.active"
+        :prepend-icon="item.action"
+        no-action
+      > -->
+      <!-- Overlapping avoidance & define icon -->
+      <v-list-group
+        v-for="(item, i) in items"
+        :key="i"
+        v-model="item.active"
+        :prepend-icon="item.icon"
+        no-action
+      >
+
+        <!-- Display the title and subtitle using for statement -->
+        <template v-slot:activator>
           <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
+            <v-list-item-title v-text="item.title"></v-list-item-title>
+          </v-list-item-content>
+        </template>
+
+        <v-list-item
+          v-for="subItem in item.subItems"
+          :key="subItem.title"
+          :to="subItem.to"
+        >
+          <v-list-item-content>
+            <v-list-item-title v-text="subItem.title"></v-list-item-title>
           </v-list-item-content>
         </v-list-item>
-      </v-list>
-  </div>
+      </v-list-group>
+    </v-list>
+  </v-card>
 </template>
 
 <script>
 export default {
   data () {
     return {
-      items: [
-        { title: 'Dashboard', icon: 'mdi-view-dashboard' },
-        { title: 'Dashboard', icon: 'mdi-view-dashboard' },
-        { title: 'Photos', icon: 'mdi-image' },
-        { title: 'About', icon: 'mdi-help-box' }
-      ],
-      right: null
+      items:
+      [
+        {
+          title: 'HOME',
+          active: true,
+          icon: 'mdi-home',
+          subItems:
+          [
+            {
+              title: 'Dashboard',
+              to: '/'
+            },
+            {
+              title: 'About',
+              to: '/about'
+            }
+          ]
+        },
+        {
+          title: 'ABOUT',
+          // active: true,
+          icon: 'mdi-information-outline',
+          to: '/About'
+        }
+      ]
     }
   }
 }
