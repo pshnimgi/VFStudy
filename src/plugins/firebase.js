5e7a73347ad1a88e18fb87f775bc2a9f7a6aa61f
pshnimgi <pshnimgo@gmail.com>
2021-04-22 18:29:07 +0900

    RealTimeDataBase setting 20210421

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
-import * as firebase from 'firebase/app'
+// import * as firebase from 'firebase/app'
+import { firebase } from '@firebase/app'
 import 'firebase/auth'
 import 'firebase/firebase-database'
+import 'firebase/firestore'
 // import vuetify from './vuetify'
 import firebaseConfig from '../../firebaseConfig'
 
